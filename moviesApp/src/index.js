c4ad787e946ef83ebb4a08aeb48852720613865b
Tomas O Dalaigh <tom1996.18@gmail.com>
2022-07-02 19:21:20 +0100

    Added routing and live data from API.

--- moviesApp/src/index.js
+++ moviesApp/src/index.js
@@ -1,95 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import MovieDetailsPage from './pages/movieDetailsPage'
-
-const images = [
-    "/2W4ZvACURDyhiNnSIaFPHfNbny3.jpg",
-    "/ewMNLXgDyiyaBGdCzQqCF8hKWy2.jpg",
-]
-
-const sample = {
-    adult: false,
-    backdrop_path: "/5Iw7zQTHVRBOYpA0V6z0yypOPZh.jpg",
-    belongs_to_collection: {
-        id: 10,
-        name: "Star Wars Collection",
-        poster_path: "/iTQHKziZy9pAAY4hHEDCGPaOvFC.jpg",
-        backdrop_path: "/d8duYyyC9J5T825Hg7grmaabfxQ.jpg",
-    },
-    budget: 200000000,
-    genres: [
-        {
-            id: 878,
-            name: "Science Fiction",
-        },
-        {
-            id: 28,
-            name: "Action",
-        },
-    ],
-    homepage:
-        "https://www.starwars.com/films/star-wars-episode-viii-the-last-jedi",
-    id: 181808,
-    imdb_id: "tt2527336",
-    original_language: "en",
-    original_title: "Star Wars: The Last Jedi",
-    overview:
-        "When a virus threatens to turn the new now earth-dwelling friendly alien hybrids against humans, Captain Rose Corley must lead a team of elite mercenaries on a mission to the alien world in order to save what's left of humanity. ",
-    popularity: 44.208,
-    poster_path: "/kOVEVeg59E0wsnXmF9nrh6OmWII.jpg",
-    production_companies: [
-        {
-            id: 1,
-            logo_path: "/o86DbpburjxrqAzEDhXZcyE8pDb.png",
-            name: "Lucasfilm",
-            origin_country: "US",
-        },
-        {
-            id: 11092,
-            logo_path: null,
-            name: "Ram Bergman Productions",
-            origin_country: "US",
-        },
-        {
-            id: 2,
-            logo_path: "/wdrCwmRnLFJhEoH8GSfymY85KHT.png",
-            name: "Walt Disney Pictures",
-            origin_country: "US",
-        },
-    ],
-    production_countries: [
-        {
-            iso_3166_1: "Fr",
-            name: "France",
-        },
-        {
-            iso_3166_1: "Li",
-            name: "Lithuania",
-        },
-    ],
-    release_date: "2020-10-25",
-    revenue: 0,
-    runtime: 113,
-    spoken_languages: [
-        {
-            iso_639_1: "en",
-            name: "English",
-        },
-    ],
-    status: "Released",
-    tagline: "To Save our world she must invade theirs.",
-    title: "Star Wars: The Last Jedi",
-    video: false,
-    vote_average: 5.7,
-    vote_count: 178,
-};
-
-const movies = [sample, sample, sample, sample, sample, sample, sample];
+import { BrowserRouter, Route, Navigate, Routes } from "react-router-dom";
+import HomePage from "./pages/homePage";
+import MoviePage from "./pages/movieDetailsPage";
 
 const App = () => {
-    return (
-        <MovieDetailsPage movie={sample} images={images} />
-    );
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/movies/:id" element={<MoviePage />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 ReactDOM.render(<App />, document.getElementById("root"));
