2b8322b6d8d06a7d1075d179c0bc6ed5b2d53472
Tomas O Dalaigh <tom1996.18@gmail.com>
2022-07-17 13:07:44 +0100

    Fix upcoming movies page.

--- moviesApp/src/pages/upcomingMoviesPage.js
+++ moviesApp/src/pages/upcomingMoviesPage.js
@@ -1,29 +1,31 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import PageTemplate from '../components/templateMovieListPage';
 import { getUpcomingMovies } from "../api/tmdb-api";
-
+import { useQuery } from 'react-query'; 
+import Spinner from '../components/spinner';
+import AddToFavouritesIcon from '../components/cardIcons/addToFavourites';
 
 const UpcomingMoviesPage = (props) => {
-  const [movies, setMovies] = useState([]);
+  const { data, error, isLoading, isError } = useQuery('movie', getUpcomingMovies)
+
   const addToFavourites = () => null;
-  const favourites = movies.filter(m => m.favourite)
-  localStorage.setItem('favourites', JSON.stringify(favourites))
+  if (isLoading) {
+    return <Spinner />
+  }
 
- // const addToFavourites = () => null;
-
-  useEffect(() => {
-    getUpcomingMovies().then(movies => {
-      setMovies(movies);
-    });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  if (isError) {
+    return <h1>{error.message}</h1>
+  }  
+  const movies = data.results;
 
   return (
     <PageTemplate
       title='Upcoming Movies'
       movies={movies}
+      action={(movie) => {
+        return <AddToFavouritesIcon movie={movie} />
+      }}
       selectFavourite={addToFavourites}
-     // selectFavourite={addToFavourites}
     />
   );
 };
